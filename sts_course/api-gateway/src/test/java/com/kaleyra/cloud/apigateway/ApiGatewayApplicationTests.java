d0a6710cb036b25d3f543852193d13d68bdcb6a2
Andrea Colleoni <andrea@colleoni.info>
2019-10-30 12:21:37 +0100

    aggiunto api gateway

---
+++ sts_course/api-gateway/src/test/java/com/kaleyra/cloud/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaleyra.cloud.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
