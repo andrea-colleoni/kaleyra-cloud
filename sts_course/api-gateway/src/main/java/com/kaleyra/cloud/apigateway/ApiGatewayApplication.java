d73dfda3b472c02cfff31c237290d65bae6fa48e
Andrea Colleoni <andrea@colleoni.info>
2019-10-30 13:01:59 +0100

    config gateway (still doesn't work)

--- sts_course/api-gateway/src/main/java/com/kaleyra/cloud/apigateway/ApiGatewayApplication.java
+++ sts_course/api-gateway/src/main/java/com/kaleyra/cloud/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,27 @@ package com.kaleyra.cloud.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
-
+	// https://cloud.spring.io/spring-cloud-gateway/multi/multi__actuator_api.html
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("r1", r ->
+//					r.host("localhost", "127.0.0.1")
+//					.and()
+					r.path("/greeting/")
+					.uri("http://localhost:8081")
+				)
+				.build();
 	}
 
 }
